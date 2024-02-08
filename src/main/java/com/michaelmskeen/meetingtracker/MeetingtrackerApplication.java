aad627b284b8a4b09676d84ec344086de9e3c080
Michael Skeen <mmskeen@gmail.com>
2024-02-08 01:12:55 -0500

    initial commit w spring boot

---
+++ src/main/java/com/michaelmskeen/meetingtracker/MeetingtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.michaelmskeen.meetingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingtrackerApplication.class, args);
+	}
+
+}
