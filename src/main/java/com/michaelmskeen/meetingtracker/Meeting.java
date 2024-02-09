82e1d7c441bf14487d25f84539d2450322bbdff9
Michael Skeen <mmskeen@gmail.com>
2024-02-08 23:00:49 -0500

    starting to add database files

---
+++ src/main/java/com/michaelmskeen/meetingtracker/Meeting.java
@@ -0,0 +1,24 @@
+package com.michaelmskeen.meetingtracker;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Meeting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String description;
+    private String whoWith;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    // Getters and Setters
+}
